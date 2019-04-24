fafe312f603474bc016a10d8cff0da680664d8a2
Jeremy <Jeremy.stevnes@gmail.com>
2019-04-24 14:30:28 -0300

    Add Function
    Display Message and Display Message in reverse

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ArrayListSimpleEx
 {
     public partial class Form1 : Form
     {
+        ArrayList phrase = new ArrayList();
+        bool forwardOrder = true;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            phrase.Add("I");
+            phrase.Add("Love");
+            phrase.Add("Programming");
+            phrase.Add("So");
+            phrase.Add("Much");
+
+        }
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            if (!forwardOrder)
+            {
+                phrase.Reverse();
+                forwardOrder = !forwardOrder;
+
+            }
+
+            lblMessage.Text = string.Empty;
+            printPhrase();
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            if (forwardOrder)
+            {
+                phrase.Reverse();
+                forwardOrder = !forwardOrder;
+            }
+
+            lblMessage.Text = string.Empty;
+            printPhrase();
+
+        }
+
+        private void printPhrase()
+        {
+            foreach (object word in phrase)
+            {
+                lblMessage.Text += word + " ";
+            }
         }
     }
 }
